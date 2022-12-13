604214dda9e1203a35eec168a069df072578dd4a
Robert Andersson <rbrtandersson@gmail.com>
2022-12-13 18:34:47 +0100

    build: simple recipe lookup, take task name as argument
    Signed-off-by: Robert Andersson <rbrtandersson@gmail.com>

--- cmd/jolt/cmd_build.go
+++ cmd/jolt/cmd_build.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/alecthomas/repr"
@@ -22,14 +23,7 @@ func init() {
 }
 
 func Build(cmd *cobra.Command, args []string) {
-	recipeSource, err := os.Open(args[0])
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer recipeSource.Close()
-
 	builder := jolt.NewBuilder()
-
 	for _, builtin := range jolt.Builtin {
 		recipe, err := builder.Parse(strings.NewReader(*builtin))
 		if err != nil {
@@ -38,27 +32,47 @@ func Build(cmd *cobra.Command, args []string) {
 		}
 	}
 
-	recipe, err := builder.Parse(recipeSource)
+	matches, err := filepath.Glob("*.jolt")
 	if err != nil {
-		repr.Println(recipe, repr.Indent("  "), repr.OmitEmpty(true))
-		panic(err)
+		panic("No .jolt files found in the current working directory")
 	}
-	// repr.Println(recipe, repr.Indent("  "), repr.OmitEmpty(true))
 
-	index := jolt.NewJobIndex()
-
-	env := recipe.Env["library"]
-	for _, task := range env.Tasks {
-		for _, job := range task.Jobs {
-			index.Add(job)
+	for _, match := range matches {
+		file, err := os.Open(match)
+		if err != nil {
+			panic("Failed to open recipe:" + err.Error())
 		}
-	}
+		defer file.Close()
 
-	schedule := jolt.NewJobSchedule(index)
-	go schedule.Dispatch()
-	<-schedule.Done()
+		recipe, err := builder.Parse(file)
+		if err != nil {
+			repr.Println(recipe, repr.Indent("  "), repr.OmitEmpty(true))
+			panic(err)
+		}
+		// repr.Println(recipe, repr.Indent("  "), repr.OmitEmpty(true))
 
-	if !index.IsEmpty() {
-		log.Fatal("Unprocessed jobs remain: ", index.RefToJob)
+		index := jolt.NewJobIndex()
+
+		for _, task := range args {
+			env, ok := recipe.Env[task]
+			if !ok {
+				panic("Task not found")
+			}
+
+			for _, task := range env.Tasks {
+				for _, job := range task.Jobs {
+					index.Add(job)
+				}
+			}
+
+			schedule := jolt.NewJobSchedule(index)
+			go schedule.Dispatch()
+			<-schedule.Done()
+
+			if !index.IsEmpty() {
+				log.Fatal("Unprocessed jobs remain: ", index.RefToJob)
+			}
+		}
+
 	}
 }
